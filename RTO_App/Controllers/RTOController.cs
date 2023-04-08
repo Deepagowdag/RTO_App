d2aaa65141826cf8cce788fa7805a461fc26501d
Deepa G <Deepa G@LAPTOP-32S6ICC8>
2023-04-08 20:26:28 +0530

    Add project files.

---
+++ RTO_App/Controllers/RTOController.cs
@@ -0,0 +1,49 @@
+﻿using Bytescout.Spreadsheet;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using RTO_App.Models;
+using System.Diagnostics;
+using System.IO;
+
+namespace RTO_App.Controllers
+{
+    public class RTOController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RTOForm model)
+        {
+            Spreadsheet document = new Spreadsheet();
+            Bytescout.Spreadsheet.Worksheet sheet = document.Workbook.Worksheets.Add("writeExcelData");
+            sheet.Cell("A1").Value = "Employee ID"; 
+            sheet.Cell("B1").Value = "Compliance Type"; 
+            sheet.Cell("C1").Value = "From"; 
+            sheet.Cell("D1").Value = "To";
+
+            sheet.Cell("A2").Value = model.EmployeeID;
+            sheet.Cell("B2").Value = model.ComplianceType;
+            sheet.Cell("C2").Value = model.From;
+            sheet.Cell("D2").Value = model.To;
+
+            if (System.IO.File.Exists(@"C:\Users\Deepa G\OneDrive\Desktop\RTOExcelData\EmployeeOutput.xlsx"))
+            {
+                System.IO.File.Delete(@"C:\Users\Deepa G\OneDrive\Desktop\RTOExcelData\EmployeeOutput.xlsx");
+            }
+            document.SaveAs(@"C:\Users\Deepa G\OneDrive\Desktop\RTOExcelData\EmployeeOutput.xlsx");
+            document.Close();
+
+            return View("Success");
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ RTO_App/Models/RTOForm.cs
@@ -0,0 +1,11 @@
+﻿namespace RTO_App.Models
+{
+    public class RTOForm
+    {
+        public int? EmployeeID { get; set; } 
+        public string? ComplianceType { get; set; }
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+
+    }
+}
